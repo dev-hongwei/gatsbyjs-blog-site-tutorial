3d4afe68da4afabf08f2c7fe5d4203abc412b332
Hongwei Liu <13699251542.l@gmail.com>
2024-03-12 22:15:43 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `gatsbyjs-blog-site-tutorial`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
